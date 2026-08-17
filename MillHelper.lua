3b368772a16c5fb7b79b0a2f1fa70da048aa619e
SrPirson <169342716+SrPirson@users.noreply.github.com>
2026-08-17 11:42:52 +0200

    fix: button fixed

--- MillHelper.lua
+++ MillHelper.lua
@@ -246,6 +246,7 @@ function MH.UpdateUI()
 
                 -- Botón Moler
                 local btn = CreateFrame("Button", nil, ui.content, "SecureActionButtonTemplate")
+                btn:RegisterForClicks("AnyUp", "AnyDown")
                 btn:SetSize(24, 24)
                 btn:SetPoint("RIGHT", cb, "RIGHT", 200, 0)
                 btn:SetNormalTexture("Interface\\Icons\\ability_miling")
